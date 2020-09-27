03dcadb3c8a32bd9f786f61eea47cb1925723a8c
Robert Neijmeijer <Robert.Neijmeijer@ahold.com>
2020-09-27 19:01:36 +0200

    Added adapter

---
+++ app/src/main/java/com/example/madlevel3task2/Portal.kt
@@ -0,0 +1,7 @@
+package com.example.madlevel3task2
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Portal(val title:String, var url:String) : Parcelable
